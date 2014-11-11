304a6259d82990af1ff5ccb7e4854665960ac8bc
Horst Gutmann <zerok@zerokspot.com>
2014-11-11 19:11:42 +0100

    Add Websocket

--- http.go
+++ http.go
@@ -4,6 +4,9 @@ import (
 	"log"
 	"net/http"
 	"sync"
+	"time"
+
+	"github.com/gorilla/websocket"
 
 	"github.com/zerok/statusd/Godeps/_workspace/src/gopkg.in/unrolled/render.v1"
 
@@ -12,6 +15,7 @@ import (
 
 var Render = render.New(render.Options{Extensions: []string{".html"}})
 var httpStatusRegistry StatusRegistry = NewStatusRegistry()
+var httpStatusRegistryManager = NewStatusRegistryManager()
 var httpStatusRegistryLock sync.RWMutex
 
 type ServerStatusModel struct {
@@ -22,6 +26,8 @@ type ServerStatusModel struct {
 type StatusOverviewModel struct {
 	Servers []ServerStatusModel
 }
+
+var upgrader = websocket.Upgrader{}
 
 // The HttpHandler sets up a HTTP endpoint to be used by 3rd parties to check if servers
 // are available or not. It is also notified of any change to the status registry in order
@@ -37,6 +43,7 @@ func HttpHandler(httpAddr string, doneGroup *sync.WaitGroup) {
 			httpStatusRegistryLock.Lock()
 			httpStatusRegistry.SetStatusFromUpdate(update)
 			httpStatusRegistryLock.Unlock()
+			httpStatusRegistryManager.SetStatus(update)
 		}
 	}()
 	statusRegistryManager.NotifyChange(statusUpdateChannel)
@@ -44,8 +51,65 @@ func HttpHandler(httpAddr string, doneGroup *sync.WaitGroup) {
 	log.Printf("Starting HTTP server on %s", httpAddr)
 	router := mux.NewRouter()
 	router.Path("/status/{server}/").HandlerFunc(httpServerStatusHandler)
+	router.Path("/overviewUpdates/").HandlerFunc(httpOverviewUpdatesHandler)
 	router.Path("/").HandlerFunc(httpFrontpageHandler)
 	http.ListenAndServe(httpAddr, router)
+}
+
+// httpOverviewUpdatesHandler offers a websocket channel that notifies the
+// receiver of updates to any registered server.
+func httpOverviewUpdatesHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		http.Error(w, "Failed to build Websocket connect", http.StatusInternalServerError)
+		return
+	}
+	updates := make(chan StatusUpdate, 5)
+	readyToExit := false
+	// Start a go-routine that drains the read messages and closes the connection
+	// if requested by the user.
+	go func() {
+		defer httpStatusRegistryManager.UnnotifyChange(updates)
+		for {
+			if _, _, err := conn.NextReader(); err != nil {
+				conn.Close()
+				readyToExit = true
+				break
+			}
+		}
+	}()
+	httpStatusRegistryManager.NotifyChange(updates)
+	defer httpStatusRegistryManager.UnnotifyChange(updates)
+overviewUpdatesLoop:
+	for {
+		if readyToExit {
+			break overviewUpdatesLoop
+		}
+		var update StatusUpdate
+		var ok bool = true
+		var updateAvailable = false
+
+		select {
+		case update, ok = <-updates:
+			updateAvailable = true
+		default:
+			// We sleep and continue in order to keep the connection open
+			// indefinitely in case no update is ever received.
+			time.Sleep(500 * time.Millisecond)
+			continue overviewUpdatesLoop
+		}
+
+		if !ok {
+			break overviewUpdatesLoop
+		}
+		if updateAvailable {
+			if err = conn.WriteJSON(update); err != nil {
+				break overviewUpdatesLoop
+			}
+			updateAvailable = false
+		}
+	}
+	conn.Close()
 }
 
 func httpFrontpageHandler(w http.ResponseWriter, r *http.Request) {
